c148503cbc56ee54a8daf45d2494b1fa76288c99
JJ_on_Jagaur <2333213@students.ac.za>
2024-04-26 14:32:56 +0200

    can find table of contents

--- Fiddles/info_extract.py
+++ Fiddles/info_extract.py
@@ -15,6 +15,7 @@ TITLE_RECT = Rect(180, 450, X_MAX, 520)
 AUTOR_RECT = Rect(180, 535, X_MAX, 620)
 
 ABSTRACT_START_TXT = "ABSTRACT"
+CONTENT_START_TXT = "CONTENTS "
 END_TXT = "_oOo_"
 
 if len(argv) == 1:
@@ -54,13 +55,41 @@ with fitz.open(file) as doc:
     page = doc[2]
     textpage = page.get_textpage()
 
-    abstract_start = page.search_for(ABSTRACT_START_TXT, textpage=textpage)
-    abstract_end = page.search_for(END_TXT, textpage=textpage)
+    # Use these two rects to locate the baounding box of the abstrct
+    abstract_start = page.search_for(ABSTRACT_START_TXT, textpage=textpage) # Directly above abstrct
+    abstract_end = page.search_for(END_TXT, textpage=textpage)              # Riectly below abstrct
 
     abstract_rect = Rect(0, abstract_start[0][3], X_MAX, abstract_end[0][1])
-    abstract_rect.bottom_left
     abstract_txt = page.get_textbox(abstract_rect).strip()
 
     print("Abstract")
     print(abstract_txt, "\n")
+
+
+    # === Page 4 === 
+    # Get table of contents 
+    page = doc[3]
+    textpage = page.get_textpage()
+
+    # Use Formatting patterns to extract the table of contents
+    content_start = page.search_for(CONTENT_START_TXT, textpage=textpage) # Directly above content table
+    content_end = page.search_for(END_TXT, textpage=textpage)              # Directly below content table
+
+    content_rect = Rect(0, content_start[0][3], X_MAX, content_end[0][1])
+    content_txt = page.get_textbox(content_rect).strip()
+
+    #print(content_txt)
+
+    content_tab = page.find_tables(clip=content_rect, strategy="text")[0]
+    rows = content_tab.extract()
+    rows = [r for r in rows if r != ['','']] # Remove empty rows
+
+    print("Contents")
+    for row in rows:
+        print(row)
+    print("\n")
+
+    # Use formatting patterns to get the issn
+    isbn_rect = page.search_for("ISBN")
+    print(isbn_rect)
 
