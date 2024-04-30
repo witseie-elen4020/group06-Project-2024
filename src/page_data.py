6206e00be636da67d9fbc99474a159c9cf1e868d
JJ_on_Jagaur <2333213@students.ac.za>
2024-04-30 16:13:23 +0200

    pageData class

--- src/page_data.py
+++ src/page_data.py
@@ -3,6 +3,7 @@ import fitz
 from fitz import Rect
 import os
 import io
+from sys import argv
 from typing import Tuple,List
 
 from extract_error import ExtractionError
@@ -35,11 +36,18 @@ class PageData():
         self.textpage = page.get_textpage()
         self.page_index = page_index
 
-        # initalis lists
-        self.fig_capts = []
-        self.table_capts = []
+        # initalis variables
+        self.raw_txt = ""       # Text contined on page: to be printed
+        self.fig_capts = []     # Holds figure caption
+        self.table_capts = []   # Holds table captions
+        self.headding = []      # Holds CAPLTALISED section headdings that occure on the page
+        self.doc_page_numer = -1    # Holds the page number as printed PDF
+        self.img_xrefs = page.get_images()  # Holds refferances to all imageson the page
+        self.abstract_start = False # Set to true iff the abstract starts in this page
+        self.content_start = False  # Set to true iff table of content starts on page
+        self.is_end = False         # Set to true iff page is the end of a section denonted by _oOo_ 
 
-        txt = self.textpage.get_text()
+        txt = self.textpage.extractText(sort=True)
         # Find chunck instead of blocks becuase this has more reliable whitesapce behavior
         chunks = txt.replace("\n ","\n").split("\n\n")
         self.raw_txt = ""
@@ -51,13 +59,37 @@ class PageData():
                 self.title = self.raw_txt
                 self.raw_txt = "" # This page need only contain the abstract
             elif chunk == END_TXT:
-                self.ended = True
+                self.is_end = True
                 self.completed = self.abstract_start
-            if chunk.startswith(FIG_TXT):
+            if chunk.startswith(FIG_TXT): # Whole Chunk will be the figure caption
                 self.fig_capts.append(chunk)
-            elif chunk.startswith(TAB_TXT):
+                continue
+            elif chunk.startswith(TAB_TXT): # Whole chunk will be table cpation
                 self.table_capts.append(chunk)
+            if(not chunk.isspace()):
+                self.raw_txt += chunk + '\n'
+        
+        # Find printed page number (if present)
+        [tmp, pg_no] = self.raw_txt.strip().rsplit('\n',1)
+        if pg_no.isnumeric():
+            self.doc_page_numer == int(pg_no)
+            self.raw_txt = f"--- PDF {page_index+1} DOC {pg_no} ---\n" + tmp
+        else:
+            self.raw_txt = f"--- PDF {page_index+1} DOC ___ ---\n" + tmp
+            
 
-            self.raw_txt += chunk
+        
+# Used for testing
+if __name__ == "__main__":
+    if len(argv) == 2:
+        print(f"Usage {argv[0]} <filename> <page-number")
+        exit()
+    file = argv[1]
+    page_no = int(argv[2])
 
-            self.fig_xrefs = page.get_images()
+    with fitz.open(file) as doc:
+        data = PageData(doc[page_no], page_no)
+        print(data.raw_txt)
+        print(f"Images:\n{data.fig_capts}")
+        print(f"image xrefs: {len(data.img_xrefs)}")
+
